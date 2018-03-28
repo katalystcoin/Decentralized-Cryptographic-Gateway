935050a5462c09dab21d0d5016957570e871e71f
Nikita Ushakov <nikita@malefici.com>
2018-03-28 18:45:08 +0300

    Init commit

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import logging
 
 from sqlalchemy import func, MetaData, Table, Column, Integer, String, DateTime, Float, Text, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -10,33 +11,6 @@ metadata = MetaData()
 
 PLATFORM_WAVES = 'waves'
 PLATFORM_ETHEREUM = 'ethereum'
-STATUS_NEW = 'new'
-STATUS_AWAITING_PROCESSING = 'awaiting_processing'
-STATUS_DONE = 'done'
-STATUS_FAILED = 'failed'
-STATUS_INVALID = 'invalid'
-
-transactions = Table('transactions', metadata,
-                     Column('id', Integer, primary_key=True),
-
-                     Column('income_tx_id', String(80)),
-                     Column('income_address', String(80)),
-                     Column('income_amount', Float),
-                     Column('income_platform', String(20)),
-                     Column('income_exchange_rate', Float),
-
-                     Column('outcome_tx_id', String(80), nullable=True),
-                     Column('outcome_address', String(80)),
-                     Column('outcome_amount', Float),
-                     Column('outcome_platform', String(20)),
-                     Column('outcome_exchange_rate', Float),
-
-                     Column('status', String(21), default=STATUS_NEW),
-                     Column('status_data', Text, nullable=True),
-
-                     Column('created_at', DateTime, server_default=func.now()),
-                     Column('updated_at', DateTime, server_default=func.now())
-                     )
 
 last_transactions = Table('last_transactions', metadata,
                           Column('id', Integer, primary_key=True),
@@ -53,5 +27,42 @@ exchange_rates = Table('exchange_rates', metadata,
                        )
 
 metadata.create_all(engine)
+
+Base = declarative_base()
+
+
+class Tx(Base):
+    STATUS_NEW = 'new'
+    STATUS_AWAITING_PROCESSING = 'awaiting_processing'
+    STATUS_DONE = 'done'
+    STATUS_FAILED = 'failed'
+    STATUS_INVALID = 'invalid'
+
+    __tablename__ = 'transactions'
+
+    income_tx_id = Column(String(80))
+    income_address = Column(String(80))
+    income_amount = Column(Float)
+    income_platform = Column(String(20))
+    income_exchange_rate = Column(Float)
+
+    outcome_tx_id = Column(String(80))
+    outcome_address = Column(String(80))
+    outcome_amount = Column(Float)
+    outcome_platform = Column(String(20))
+    outcome_exchange_rate = Column(Float)
+
+    status = Column(String(20), default=STATUS_NEW)
+    status_data = Column(Text, nullable=True)
+
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_onupdate=func.now())
+
+    def __repr__(self):
+        return "<Tx(name='%s', fullname='%s', password='%s')>" % (self.income_address,
+                                                                  self.outcome_address, self.status)
+
+
+Base.metadata.create_all(engine)
 
 database_connection = engine.connect()
