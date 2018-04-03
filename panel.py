a44365d4b3b8c7437b0fb085adbc5b0ed2575db7
Nikita Ushakov <nikita@malefici.com>
2018-04-03 18:38:48 +0300

    panel updates

--- panel.py
+++ panel.py
@@ -139,7 +139,7 @@ RATES_TEMPLATE = """
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        <title>Transactions</title>
+        <title>Exchange rates</title>
         <style>
             body { font-size: 12px; font-family: monospace; }
         </style>
